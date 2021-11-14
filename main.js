857635497da30ad97c8d1902c2b9bc3746d32053
jpduong12 <jpduong12@gmail.com>
2021-11-14 13:01:49 -0800

    Debugging

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client({ intents: [8] });
+const client = new Discord.Client({ intents: [] });
 
 const prefix = '-';
 
-client.once('ready', () => {
-    console.log('To the moon!');
+require('dotenv').config();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    /*if (!message.content.startsWith(prefix)) message.channel.send('I GOT IT!');
 
     const args = message.content.slice(prefix.length).split(/ + /);
     const command = args.shift().toLowerCase();
 
     if (command === 'ping') {
-        message.channel.send('pong!')
+        message.channel.send('pong!');
     }
     else {
         message.channel.send('Command not recognized.');
+    }*/
+
+    if (message.content === "ping") {
+        message.reply("pong");
     }
 });
 
-client.login('OTA5MzY1NTkzMjYwNTU2Mjg4.YZDOtA.uttzQQlkQDj9s6s4qexO-_kqZA0');
+client.login(process.env.token);
